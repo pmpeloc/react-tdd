90134213ed14ef66bd2ea2aa401cd04bdd09a467
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-02-26 14:30:43 -0300

    feat: React Testing Library added

---
+++ intro-react-testing-library/src/components/hello-world/hello-world.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import HelloWorld from './hello-world';
+
+test('renders hello world', () => {
+  render(<HelloWorld />);
+  const title = screen.getByText(/hello world/i);
+  expect(title).toBeInTheDocument();
+});
