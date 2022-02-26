a4f0e6c22cbcb579312ef3393b766fb64fe2f46c
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-02-26 18:02:01 -0300

    feat: Submit validations test added

--- store-app/src/form/form.js
+++ store-app/src/form/form.js
@@ -1,15 +1,45 @@
+import { useState } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import TextField from '@mui/material/TextField';
 import NativeSelect from '@mui/material/NativeSelect';
 import Button from '@mui/material/Button';
 
 const Form = () => {
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    size: '',
+    type: '',
+  });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const { name, size, type } = e.target.elements;
+    if (!name.value) {
+      setFormErrors((prevState) => ({
+        ...prevState,
+        name: 'The name is required',
+      }));
+    }
+    if (!size.value) {
+      setFormErrors((prevState) => ({
+        ...prevState,
+        size: 'The size is required',
+      }));
+    }
+    if (!type.value) {
+      setFormErrors((prevState) => ({
+        ...prevState,
+        type: 'The type is required',
+      }));
+    }
+  };
+
   return (
     <>
       <h1>Create product</h1>
-      <form>
-        <TextField label='name' id='name' />
-        <TextField label='size' id='size' />
+      <form onSubmit={submitHandler}>
+        <TextField label='name' id='name' helperText={formErrors.name} />
+        <TextField label='size' id='size' helperText={formErrors.size} />
         <InputLabel htmlFor='type'>Type</InputLabel>
         <NativeSelect
           value=''
@@ -17,11 +47,13 @@ const Form = () => {
             name: 'type',
             id: 'type',
           }}>
+          <option aria-label='None' value='' />
           <option value={'electronic'}>Electronic</option>
           <option value={'furniture'}>Furniture</option>
           <option value={'clothing'}>Clothing</option>
         </NativeSelect>
-        <Button>Submit</Button>
+        {formErrors.type.length && <p>{formErrors.type}</p>}
+        <Button type='submit'>Submit</Button>
       </form>
     </>
   );
--- store-app/src/form/form.test.js
+++ store-app/src/form/form.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable testing-library/prefer-presence-queries */
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 
 import Form from './form';
 
@@ -22,5 +22,16 @@ describe('When the form is mounted', () => {
   });
   it('Should exist the submit button', () => {
     expect(screen.getByRole('button', { name: /submit/i })).toBeInTheDocument();
+  });
+});
+
+describe('When the user submits the form without values', () => {
+  it('Should display validation messages', () => {
+    render(<Form />);
+    expect(screen.queryByText(/the name is required/i)).not.toBeInTheDocument();
+    fireEvent.click(screen.getByRole('button', { name: /submit/i }));
+    expect(screen.queryByText(/the name is required/i)).toBeInTheDocument();
+    expect(screen.queryByText(/the size is required/i)).toBeInTheDocument();
+    expect(screen.queryByText(/the type is required/i)).toBeInTheDocument();
   });
 });
