794c633c0c4f375a599f50f51a71c9d05c384d3b
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-02-26 15:48:13 -0300

    feat: Form elements test added

--- store-app/src/form/form.js
+++ store-app/src/form/form.js
@@ -1,5 +1,28 @@
+import InputLabel from '@mui/material/InputLabel';
+import TextField from '@mui/material/TextField';
+import NativeSelect from '@mui/material/NativeSelect';
+
 const Form = () => {
-  return <h1>Create product</h1>;
+  return (
+    <>
+      <h1>Create product</h1>
+      <form>
+        <TextField label='name' id='name' />
+        <TextField label='size' id='size' />
+        <InputLabel htmlFor='type'>Type</InputLabel>
+        <NativeSelect
+          value=''
+          inputProps={{
+            name: 'type',
+            id: 'type',
+          }}>
+          <option value={'electronic'}>Electronic</option>
+          <option value={'furniture'}>Furniture</option>
+          <option value={'clothing'}>Clothing</option>
+        </NativeSelect>
+      </form>
+    </>
+  );
 };
 
 export default Form;
--- store-app/src/form/form.test.js
+++ store-app/src/form/form.test.js
@@ -1,13 +1,23 @@
+/* eslint-disable testing-library/prefer-presence-queries */
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 
 import Form from './form';
 
 describe('When the form is mounted', () => {
+  // eslint-disable-next-line testing-library/no-render-in-setup
+  beforeEach(() => render(<Form />));
   it('There must be a create product form page', () => {
-    render(<Form />);
     expect(
       screen.getByRole('heading', { name: /create product/i })
     ).toBeInTheDocument();
+  });
+  it('The form must have the following fields: name, size, type (electronic, furniture, clothing).', () => {
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/size/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/type/i)).toBeInTheDocument();
+    expect(screen.queryByText(/electronic/i)).toBeInTheDocument();
+    expect(screen.queryByText(/furniture/i)).toBeInTheDocument();
+    expect(screen.queryByText(/clothing/i)).toBeInTheDocument();
   });
 });
