437417e85942f4b9665ca7a0f0322b5098dfc5cf
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-02-28 19:11:22 -0300

    feat: Styled Page added

--- github-search/src/components/github-search-page/github-search-page.js
+++ github-search/src/components/github-search-page/github-search-page.js
@@ -1,16 +1,26 @@
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 export const GitHubSearchPage = () => {
   return (
-    <>
+    <Container>
       <Typography component='h1' variant='h3'>
         GitHub repositories list
       </Typography>
-      <TextField label='Filter by' id='filterBy' />
-      <Button>Search</Button>
-    </>
+      <Grid container spacing={2} justifyContent='space-between'>
+        <Grid item md={6} xs={12}>
+          <TextField fullWidth label='Filter by' id='filterBy' />
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Button fullWidth color='primary' variant='contained'>
+            Search
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
