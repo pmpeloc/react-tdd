925ba113f1725b99e1c10ba7518a4c618fd4a4b8
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-03-01 10:37:15 -0300

    feat: Validate rows per page added

--- github-search/src/components/github-search-page/github-search-page.test.js
+++ github-search/src/components/github-search-page/github-search-page.test.js
@@ -90,4 +90,16 @@ describe('When the developer does a search', () => {
     await screen.findByRole('table');
     expect(screen.getByText(/1–1 of 1/i)).toBeInTheDocument();
   });
+  it('Results size per page select/combobox with the options: 30, 50, 100.', async () => {
+    fireClickSearch();
+    await screen.findByRole('table');
+    expect(screen.getByLabelText(/rows per page/i)).toBeInTheDocument();
+    fireEvent.mouseDown(screen.getByLabelText(/rows per page/i));
+    const listbox = screen.getByRole('listbox', { name: /rows per page/i });
+    const options = within(listbox).getAllByRole('option');
+    const [option30, option50, option100] = options;
+    expect(option30).toHaveTextContent(/30/);
+    expect(option50).toHaveTextContent(/50/);
+    expect(option100).toHaveTextContent(/100/);
+  });
 });
