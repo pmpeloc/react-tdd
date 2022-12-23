9708ac4b9a45f2e79ef5e34a8dcf89be383f102e
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-12-23 11:31:06 -0300

    chore: Restructure tests

--- github-search/src/components/github-search-page/github-search-page.test.js
+++ github-search/src/components/github-search-page/github-search-page.test.js
@@ -22,7 +22,6 @@ import {
   UNEXPECTED_STATUS,
   UNPROCESSABLE_STATUS,
 } from '../../constants';
-import { paginatedHandler } from '../../__fixtures__/handlers';
 
 const fakeResponse = makeFakeResponse({ totalCount: 1 });
 const fakeRepo = makeFakeRepo();
@@ -197,89 +196,6 @@ describe('When the developer types on filter by and does a search', () => {
     const [repository] = tableCells;
     expect(repository).toHaveTextContent(expectedRepo.name);
   });
-});
-
-describe('When the developer does a search and selects 50 rows per page', () => {
-  it('Must fetch a new search and display 50 rows results on the table', async () => {
-    server.use(rest.get('/search/repositories', paginatedHandler));
-    fireClickSearch();
-    expect(await screen.findByRole('table')).toBeInTheDocument();
-    expect(await screen.findAllByRole('row')).toHaveLength(31);
-    fireEvent.mouseDown(screen.getByLabelText(/rows per page/i));
-    fireEvent.click(screen.getByRole('option', { name: '50' }));
-    setTimeout(async () => {
-      await waitFor(() =>
-        expect(
-          screen.getByRole('button', { name: /search/i })
-        ).not.toBeDisabled()
-      );
-      expect(screen.getAllByRole('row')).toHaveLength(51);
-    }, 30000);
-  }, 50000);
-});
-
-describe('When the developer clicks on search and then on next page button', () => {
-  it('must display the next repositories page', async () => {
-    // Config server handler
-    server.use(rest.get('/search/repositories', paginatedHandler));
-    // Click search
-    fireClickSearch();
-    // Wait table
-    expect(await screen.findByRole('table')).toBeInTheDocument();
-    // Expect fist repo name is form page 0
-    expect(screen.getByRole('cell', { name: /1-0/ })).toBeInTheDocument();
-    // Expect next page is not disabled
-    expect(
-      screen.getByRole('button', { name: /next page/i })
-    ).not.toBeDisabled();
-    // Click next page button
-    fireEvent.click(screen.getByRole('button', { name: /next page/i }));
-    // Wait search button is not disabled
-    expect(screen.getByRole('button', { name: /search/i })).toBeDisabled();
-    await waitFor(
-      () =>
-        expect(
-          screen.getByRole('button', { name: /search/i })
-        ).not.toBeDisabled(),
-      { timeout: 3000 }
-    );
-    // Expect first repo name is from page 1
-    expect(screen.getByRole('cell', { name: /2-0/ })).toBeInTheDocument();
-  }, 30000);
-});
-
-describe('When the developer clicks on search and then on next page button and then on previous page button', () => {
-  it('Must display the previous repositories page', async () => {
-    server.use(rest.get('/search/repositories', paginatedHandler));
-    fireClickSearch();
-    expect(await screen.findByRole('table')).toBeInTheDocument();
-    expect(screen.getByRole('cell', { name: /1-0/ })).toBeInTheDocument();
-    expect(
-      screen.getByRole('button', { name: /next page/i })
-    ).not.toBeDisabled();
-    fireEvent.click(screen.getByRole('button', { name: /next page/i }));
-    expect(screen.getByRole('button', { name: /search/i })).toBeDisabled();
-    await waitFor(
-      () =>
-        expect(
-          screen.getByRole('button', { name: /search/i })
-        ).not.toBeDisabled(),
-      { timeout: 3000 }
-    );
-    expect(screen.getByRole('cell', { name: /2-0/ })).toBeInTheDocument();
-    // Click previous page
-    fireEvent.click(screen.getByRole('button', { name: /previous page/i }));
-    // Wait search finish
-    await waitFor(
-      () =>
-        expect(
-          screen.getByRole('button', { name: /search/i })
-        ).not.toBeDisabled(),
-      { timeout: 3000 }
-    );
-    // Expect
-    expect(screen.getByRole('cell', { name: /1-0/ })).toBeInTheDocument();
-  }, 30000);
 });
 
 describe('When there is an unprocessable error 422 from the backend', () => {
