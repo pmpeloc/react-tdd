9a9ff800aac6a1ce26f30e5c98a3d38f01b54192
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-12-27 23:18:20 -0300

    feat: Refactor UI Employee added

--- auth-company/src/employee/components/employee-page/employee-page.js
+++ auth-company/src/employee/components/employee-page/employee-page.js
@@ -1,15 +1,19 @@
 /* eslint-disable react/function-component-definition */
 import React, { useContext } from 'react';
+import { Button, Typography } from '@mui/material';
 
 import { AuthContext } from '../../../utils/contexts/auth-context';
+import { ADMIN_ROLE } from '../../../consts';
 
 export const EmployeePage = () => {
   const { user } = useContext(AuthContext);
 
   return (
     <>
-      <h1>Employee Page</h1>
-      {user.role === 'admin' && <button type="button">Delete</button>}
+      <Typography component="h1" variant="h5">
+        Employee Page
+      </Typography>
+      {user.role === ADMIN_ROLE && <Button type="button">Delete</Button>}
     </>
   );
 };
